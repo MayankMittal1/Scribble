728630faf6f0035c7a3bc139f77db52a9caf53aa
mayankmittal-iitr <mmittal123456@gmail.com>
2021-05-01 17:15:37 +0530

    port

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const port = process.env.PORT || 3000;
 var drawing=[]
 var users=[]
 var i=0
@@ -132,6 +133,6 @@ io.on('connection', (socket) => {
   })
 });
 
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log('listening on *:3000');
 });
